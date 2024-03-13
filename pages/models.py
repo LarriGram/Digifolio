0914c03cee7b6bcc92da65abc062f3bacbce18e7
LarriGram <lgraham@clarku.edu>
2024-03-12 23:51:26 -0400

    Changed URL, Views and Forms Pages, added gitignore

---
+++ pages/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    technology = models.CharField(max_length=20)
+
+#Takes in a name and info
+class Portfolio(models.Model):
+    FirstName = models.CharField(max_length=100)
+    LastName = models.TextField(max_length=100)
+    Major = models.CharField(max_length=20, default = "")
+    Minor = models.CharField(max_length = 20, default = "")
+    Year = models.IntegerField()
+    Resume = models.FileField(upload_to='resumes/', default='default_resume.pdf')
+    Email = models.CharField(max_length=100, default = "")
+    LinkedIn = models.URLField(max_length=100, default = "")
+    GitHub = models.URLField(max_length = 100, default = "")
